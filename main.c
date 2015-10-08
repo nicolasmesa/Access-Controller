d716fd932a28ca2c84a7a1e5edd578b6e1feed07
Nicolas Mesa <nicolasmesa@gmail.com>
2015-10-08 18:29:32 -0400

    Adding ACL to files

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ struct user_struct {
 	char *username;
 	struct user_struct *next; // Only used to traverse all users
 	struct user_group_list *groups;
+	struct file_struct *file;
 };
 
 struct group_struct {
@@ -44,7 +45,7 @@ struct acl_entry {
 	struct user_struct *user;
 	int readPermission;
 	int writePermission;
-}
+};
 
 
 static struct file_struct *root;
@@ -64,7 +65,7 @@ void printAndExit(char *msg) {
 struct file_struct *findFileInListByName(struct file_struct *file, char *cmpName) {
 	struct file_struct *curr = file;
 
-	while (curr) {
+	while (curr != NULL) {
 		if (strncmp(curr->cmpName, cmpName, MAX_CMP_SIZE) == 0 ) {
 			return curr;
 		}
@@ -75,22 +76,19 @@ struct file_struct *findFileInListByName(struct file_struct *file, char *cmpName
 	return NULL;
 }
 
-int addChild(struct file_struct *parent, struct file_struct *child) {
-        if (findFileInListByName(parent->children, child->cmpName)) {
-                printf("Error: File name already exists (%s)", child->cmpName);
-                exit(1);
-        }
+int addChildFile(struct file_struct *parent, struct file_struct *child) {
+    if (findFileInListByName(parent->children, child->cmpName)) {
+            printAndExit("Error: File name already exists");            
+    }
 
-        child->next = parent->children;
-        parent->children = child;
+    child->next = parent->children;
+    parent->children = child;
 
 	return 0;
 }
 
 struct file_struct *createFile(char *cmpName, struct file_struct *parent) {
 	struct file_struct *file = malloc(sizeof(struct file_struct));
-
-	printf("Creating file %s\n", cmpName);
 
 	if (!file) {
 		printAndExit(NULL);
@@ -106,13 +104,65 @@ struct file_struct *createFile(char *cmpName, struct file_struct *parent) {
 	file->cmpName[MAX_CMP_SIZE] = '\0';
 
 	if (parent) {
-		addChild(parent, file);
+		addChildFile(parent, file);
 	}
 
 	return file;
 }
 
+struct file_struct *findFileByPath(char *pathStart) {
+	char cmpName[MAX_CMP_SIZE + 1];	
+	char *path = pathStart;	
+	struct file_struct *currentFile = root;
+	int last = 0;
 
+	if (!path) {
+		printAndExit("Undefined path");
+	}
+
+	if (*path != '/') {
+		printAndExit("File path must start with /");
+	}
+
+	path++;
+
+	while (*path != '\0') {
+		int cmpLength = 0;
+
+		while (*path != '/') {
+			if (*path == '\0') {
+				last = 1;				
+				break;
+			}
+
+			cmpName[cmpLength] = *path;
+			cmpLength++;
+			path++;
+
+			if (cmpLength > MAX_CMP_SIZE) {
+				cmpName[MAX_CMP_SIZE] = '\0';
+				printAndExit("Component longer than allowed");
+			}
+		}
+
+		cmpName[cmpLength] = '\0';
+
+
+		currentFile = findFileInListByName(currentFile->children, cmpName);
+
+		if (currentFile == NULL) {			
+			printAndExit("File does not exist");
+		}
+
+		if (last) {
+			break;
+		}
+
+		path++;
+	}
+
+	return currentFile;	
+}
 
 struct file_struct *addFileByPath(char *pathStart) {
 	char cmpName[MAX_CMP_SIZE + 1];	
@@ -221,9 +271,10 @@ struct user_struct *createUser(char *username) {
 		printAndExit(NULL);
 	}
 
-	user->username = username;
+	user->username = strdup(username);
 	user->next = usersHead;
 	user->groups = NULL;
+	user->file = NULL;
 
 	usersHead = user;	
 
@@ -243,7 +294,7 @@ struct group_struct *createGroup(char *groupname) {
 		printAndExit(NULL);
 	}
 
-	group->groupname = groupname;
+	group->groupname = strdup(groupname);
 	group->next = groupsHead;
 	group->users = NULL;
 
@@ -289,8 +340,6 @@ void addUserToGroup(struct user_struct *user, struct group_struct *group) {
 	struct user_struct *groupUser = findGroupUser(group, user->username);
 
 	if(userGroup == NULL) {
-		printf("Adding group %s to user %s \n", group->groupname, user->username);
-
 		struct user_group_list *userGroupContainer = malloc(sizeof(struct user_group_list));
 
 		if (userGroupContainer == NULL) {
@@ -300,14 +349,10 @@ void addUserToGroup(struct user_struct *user, struct group_struct *group) {
 		userGroupContainer->group = group;
 		userGroupContainer->next = user->groups;
 		user->groups = userGroupContainer;
-	} else {
-		printf("User %s already has group %s\n", user->username, group->groupname);		
 	}
 
 
 	if (groupUser == NULL) {	
-		printf("Adding user %s to group %s \n", user->username, group->groupname);
-
 		struct group_user_list *groupUserContainer = malloc(sizeof(struct group_user_list));
 
 		if (groupUserContainer == NULL) {
@@ -317,8 +362,6 @@ void addUserToGroup(struct user_struct *user, struct group_struct *group) {
 		groupUserContainer->user = user;
 		groupUserContainer->next = group->users;
 		group->users = groupUserContainer;
-	} else {
-		printf("Group %s already has user %s \n", group->groupname, user->username);
 	}
 }
 
@@ -328,34 +371,119 @@ int addUserAndGroup(char *username, char *groupname) {
 
 	if (user == NULL) {
 		user = createUser(username);
-	} else {
-		free(username);
 	}
 
 
 	if (group == NULL) {
 		group = createGroup(groupname);
-	} else {
-		free(groupname);
 	}
-
 
 	addUserToGroup(user, group);
 
-
 	return 0;
-	// if any of them is not found, free memory for the string (username or groupname)
+}
+
+void validatePermissions(char *permissions){
+	if (permissions == NULL) {
+		printAndExit("Permissions are invalid");
+	}
+
+	int len = strlen(permissions);
+
+	if (len > 2) {
+		printAndExit("Permissions are invalid");
+	}
+
+	if (*permissions == '-' && len > 1){
+		printAndExit("Permissions are invalid");
+	}
+
+	if (len == 2 && (permissions[0] != 'r' || permissions[1] != 'w')) {
+		printAndExit("Permissions are invalid");
+	}
+
+	if (len == 1 && (permissions[0] != 'r' && permissions[0] != 'w')) {
+		printAndExit("Permissions are invalid");
+	}
+}
+
+struct acl_entry *createAclEntry(char *permissions, struct user_struct *user, struct group_struct *group) {
+	validatePermissions(permissions);
+
+	int len = strlen(permissions);
+	struct acl_entry *aclEntry = malloc(sizeof(struct acl_entry));
+
+	if (aclEntry == NULL) {
+		printAndExit(NULL);
+	}
+
+	aclEntry->next = NULL;
+	aclEntry->group = group;
+	aclEntry->user = user;
+	aclEntry->readPermission = 0;
+	aclEntry->writePermission = 0;
+
+	if (len == 1) {
+		if (*permissions == 'r') {
+			aclEntry->readPermission = 1;
+		}
+
+		if (*permissions == 'w') {
+			aclEntry->writePermission = 1;
+		}
+	}
+
+	if (len == 2) {
+		aclEntry->readPermission = 1;
+		aclEntry->writePermission = 1;
+	}
+
+	return aclEntry;
+}
+
+struct acl_entry *findAclByFileUserAndGroup(struct file_struct *file, struct user_struct *user, struct group_struct *group){
+	struct acl_entry *aclEntry;
+
+	for (aclEntry = file->aclHead; aclEntry != NULL; aclEntry = aclEntry->next) {
+		if (aclEntry->user == user && aclEntry->group == group) {
+			return aclEntry;
+		}
+	}
+
+	return NULL;
+}
+
+void addAclToFile(struct file_struct *file, char *permissions, struct user_struct *user, struct group_struct *group) {		
+	if (findAclByFileUserAndGroup(file, user, group)) {
+		printf("File already had ACL for that group and user\n");
+		// Adding another entry for the same user and
+		// group doesn't make sense since the first one
+		// is the one that counts. 
+		// @todo validate this
+		return;
+	}
+
+	struct acl_entry *aclEntry = createAclEntry(permissions, user, group);
+
+	if (file->aclTail == NULL){
+		file->aclTail = file->aclHead = aclEntry;
+		return;
+	}
+
+	file->aclTail->next = aclEntry;
+	file->aclTail = aclEntry;
 }
 
 int parseUserDefinitionLine(char *line) {
 	char *userStart = line;
 	char *groupStart;
 	char *filePathStart;
-	char *fileName;
+	char *fileName = NULL;
+	struct user_struct *user;
+	struct group_struct *group;
+	struct user_struct *possibleUser;
 	char c;
 	int len = 0;
-
-	printf("Line = %s\n", line);
 
 	//@todo *.group, user.*, *.*
 
@@ -375,7 +503,8 @@ int parseUserDefinitionLine(char *line) {
 	}
 
 	char *username = strndup(userStart, len);
-	struct user_struct *possibleUser = findUserByUsername(username);
+
+	possibleUser = findUserByUsername(username);
 
 	int noFile = 0;
 
@@ -399,11 +528,19 @@ int parseUserDefinitionLine(char *line) {
 
 	addUserAndGroup(username, groupname);
 
+	user = findUserByUsername(username);
+	group = findGroupByGroupname(groupname);
+
 
 	if (noFile) {
 		if (possibleUser == NULL) {
 			printAndExit("The first instance of a user must have a file name");
 		}
+
+		addAclToFile(user->file, "rw", user, group);
+
+		free(groupname);
+		free(username);
 
 		return 0;
 	}
@@ -419,29 +556,42 @@ int parseUserDefinitionLine(char *line) {
 		line++;
 	}
 
+	// File name
 	if (len) {
 		if (possibleUser != NULL) {
 			printAndExit("Only the first instance of the user can contain a file");		
 		}
 
-		fileName = strndup(filePathStart, len);
-		addFileByPath(fileName);		
+		fileName = strndup(filePathStart, len);		
+		user->file = addFileByPath(fileName);
 	} else {
 		if (possibleUser == NULL) {			
 			printAndExit("The first instance of a user must have a file name");	
 		}		
 	}
 
+	addAclToFile(user->file, "rw", user, group);
+
+
+	if (fileName != NULL) {
+		free(fileName);
+	}
+
+	free(groupname);
+	free(username);
+
 	return 0;
 }
 
 int initFs() {
 	root = createFile("/", NULL);
-	createFile("tmp", root);
-	createFile("home", root);
-	addFileByPath("/home/nmesa");
-	addFileByPath("/home/nmesa/hola/como/estas");	
-	addFileByPath("/tmp/nicolas/mesa");
+
+	struct file_struct *tmp = createFile("tmp", root);
+	struct file_struct *home = createFile("home", root);
+
+	addAclToFile(root, "r", NULL, NULL);
+	addAclToFile(tmp, "rw", NULL, NULL);
+	addAclToFile(home, "r", NULL, NULL);
 
 	return 0;
 }
@@ -499,10 +649,59 @@ int parseUserDefinitionSection() {
 	return 0;
 }
 
+
+void printAclForFile(struct file_struct *file) {
+	printf("ACL for file %s\n", file->cmpName);
+	struct acl_entry *aclEntry;
+
+	for (aclEntry = file->aclHead; aclEntry != NULL; aclEntry = aclEntry->next) {
+		struct user_struct *user = aclEntry->user;
+		struct group_struct *group = aclEntry->group;
+		char permissions[3];
+
+		char *username;
+		char *groupname;
+
+		if (user != NULL) {
+			username = user->username;
+		} else {
+			username = "*";
+		}
+
+		if (group != NULL) {
+			groupname = group->groupname;
+		} else {
+			groupname = "*";
+		}
+
+		if (aclEntry->readPermission && aclEntry->writePermission) {
+			permissions[0] = 'r';
+			permissions[1] = 'w';
+			permissions[2] = '\0';
+		} else if(aclEntry->readPermission) {
+			permissions[0] = 'r';
+			permissions[1] = '\0';
+		} else if (aclEntry->writePermission) {
+			permissions[0] = 'w';
+			permissions[1] = '\0';
+		} else {
+			permissions[0] = '-';
+			permissions[1] = '\0';
+		}		
+
+		printf("\t%s.%s %s\n", username, groupname, permissions);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	initFs();
 
+	printAclForFile(root);
+
 	parseUserDefinitionSection();
+
+	struct file_struct *file = findFileByPath("/home/smb");
+	printAclForFile(file);
 
 	return 0;
 }
